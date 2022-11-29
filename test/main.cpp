3361c030f75db21a6ed29a9747980efb72efed86
sin <qgwang@telenav.cn>
2022-11-29 18:33:52 +0800

    set argv

--- test/main.cpp
+++ test/main.cpp
@@ -11,22 +11,22 @@ int main( int argc, char *argv[], char *env[])
     int i = 0;
     while(argv[i])
     {
-	    std::cout << argv[i] << std::endl;
+	    std::cout << i << ">>" << argv[i] << std::endl;
 	    i++;
     }
     std::cout << "--------------END ARGV------ BEGIN OUT PUT ENV----------------" << std::endl;
-	int j = 0;
+	  int j = 0;
     while(env[j])
     {
 	    std::cout << env[j++] << std::endl;
     }
     std::cout << "===========END ENV===========" << std::endl;
-    if(argv[1])
+    if(argc > 1)
     {
 	    std::cout << "argv[1] = " << argv[1] << std::endl;
       const char *workdir = "/home/nvidia/workspace/wqg/QingLong";
       chdir(workdir);
-	    int ret = execve(argv[1], argv, env); 
+	    int ret = execve(argv[1], &argv[1], env); 
     	if (  -1 == ret )  
     	{ 
 	    	std::cout << "failed to execve" << std::endl; 
