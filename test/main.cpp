9624e9586a1ba20f2af1eb59a7f6ed402ad2948b
sin <qgwang@telenav.cn>
2022-11-29 18:42:02 +0800

    fork child process

--- test/main.cpp
+++ test/main.cpp
@@ -23,16 +23,29 @@ int main( int argc, char *argv[], char *env[])
     std::cout << "===========END ENV===========" << std::endl;
     if(argc > 1)
     {
-	    std::cout << "argv[1] = " << argv[1] << std::endl;
-      const char *workdir = "/home/nvidia/workspace/wqg/QingLong";
-      chdir(workdir);
-	    int ret = execve(argv[1], &argv[1], env); 
-    	if (  -1 == ret )  
-    	{ 
-	    	std::cout << "failed to execve" << std::endl; 
-        	perror( "execve" );  
-        	exit( EXIT_FAILURE);  
-    	}  
+      pid_t childPid = 0;
+      childPid = fork();
+      if(childPid == 0)
+	    {
+        std::cout << "argv[1] = " << argv[1] << std::endl;
+        const char *workdir = "/home/nvidia/workspace/wqg/QingLong/";
+        chdir(workdir);
+        int ret = execve(argv[1], &argv[1], env); 
+        if (  -1 == ret )  
+        { 
+          std::cout << "failed to execve" << std::endl; 
+            perror( "execve" );  
+            exit( EXIT_FAILURE);  
+        }
+        else
+        {
+          std::cout << "success to execve " << workdir << argv[1] << std::endl;
+        }
+      }
+      else
+      {
+        std::cout << "main create childPid = " << childPid << std::endl;
+      }
     }
     puts( "shouldn't get here" );  
     exit( EXIT_SUCCESS );  
