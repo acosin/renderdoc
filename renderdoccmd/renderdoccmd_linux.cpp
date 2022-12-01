f79090bcb69004f7f9b4bc4816cf530bff2e4107
sin <qgwang@telenav.cn>
2022-12-01 13:28:12 +0800

    test execve

--- renderdoccmd/renderdoccmd_linux.cpp
+++ renderdoccmd/renderdoccmd_linux.cpp
@@ -322,8 +322,55 @@ void sig_handler(int signo)
     exit(1);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char* env[])
 {
+    char * args[] = {(char*) "/home/nvidia/workspace/wqg/QingLong/hmi",(char*) NULL };  
+  	std::cout << "argc = " << argc << std::endl;
+    std::cout << "-------------------BEGIN OUTPUT ARGV-----------------" << std::endl;
+    int i = 0;
+    while(argv[i])
+    {
+	    std::cout << i << ">>" << argv[i] << std::endl;
+	    i++;
+    }
+    std::cout << "--------------END ARGV------ BEGIN OUT PUT ENV----------------" << std::endl;
+	  int j = 0;
+    while(env[j])
+    {
+	    std::cout << env[j++] << std::endl;
+    }
+    std::cout << "===========END ENV===========" << std::endl;
+    if(argc > 1)
+    {
+      pid_t childPid = 0;
+      childPid = fork();
+      if(childPid == 0)
+	    {
+        std::cout << "argv[1] = " << argv[1] << std::endl;
+        const char *workdir = "/home/nvidia/workspace/wqg/QingLong/";
+        chdir(workdir);
+        std::cout << "change workdir = " << workdir << std::endl;
+        int ret = execve(argv[1], &argv[1], env); 
+        if (  -1 == ret )  
+        { 
+          std::cout << "failed to execve" << std::endl; 
+            perror( "execve" );  
+            exit( EXIT_FAILURE);  
+        }
+        else
+        {
+          std::cout << "success to execve " << workdir << argv[1] << std::endl;
+        }
+      }
+      else
+      {
+        std::cout << "main create childPid = " << childPid << std::endl;
+      }
+    }
+    puts( "shouldn't get here" );  
+    exit( EXIT_SUCCESS );  
+  return 0;
+#ifdef USE_RENDER_DOC_CMD
   setlocale(LC_CTYPE, "");
 
   signal(SIGINT, sig_handler);
@@ -417,6 +464,6 @@ int main(int argc, char *argv[])
   if(display)
     XCloseDisplay(display);
 #endif
-
-  return ret;
+return ret;
+#endif //USE_RENDER_DOC_CMD
 }
